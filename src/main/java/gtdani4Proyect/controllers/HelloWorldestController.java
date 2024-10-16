97a9ba5a21c164268285d1a4c49557f134f7df46
Daniel Gutiérrez Torres <82153822+guti10x@users.noreply.github.com>
2024-10-16 12:56:11 +0200

    Add Helloworld con @

--- src/main/java/gtdani4Proyect/controllers/HelloWorldestController.java
+++ src/main/java/gtdani4Proyect/controllers/HelloWorldestController.java
@@ -1,8 +1,38 @@
 package gtdani4Proyect.controllers;
 
-@Restcontroller
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
 public class HelloWorldestController {
 
-     
+    @GetMapping("/hello")
+    public String helloWorld() {
+        System.out.println("Hello World escrito en consola");
+        return "Hello World";
+    }
 
+    @GetMapping("/hello/{name}")
+    public String helloPerson(@PathVariable String name) {
+        if (isPalindrome(name)) {
+            return "Hello " + name + "Tu nombre es un palíndromo";
+        } else {
+            return "Hello " + name + " Tu nombre es un palíndromo";
+        }
+    }
+
+
+
+    private boolean isPalindrome(String name) {
+        name = name.toLowerCase();  
+        int length = name.length();
+
+        for (int i = 0; i < length / 2; i++) {
+            if (name.charAt(i) != name.charAt(length - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
